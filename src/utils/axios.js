377307f46d8cb5d730f4c2383a6e51061faa2dde
muftira <muh.muftir@gmail.com>
2023-02-27 09:48:27 +0700

    fix: axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 // const base_url = 'http://localhost:5500/api'
-const base_url = "https://checkin-invitation-be.vercel.app/api";
+const base_url = "https://checkin-invitation-be.vercel.app/";
 
-export const getData = () => axios.get(base_url + "/guest");
+export const getData = () => axios.get(base_url + "api/guest");
 
 export const postData = (name, address) =>
-  axios.post(base_url + "/guest", { name, address });
+  axios.post(base_url + "api/guest", { name, address });
 
-export const deleteData = (id) => axios.delete(base_url + "/guest/" + id);
+export const deleteData = (id) => axios.delete(base_url + "api/guest/" + id);
 
 export const updateData = (id, name, address) =>
-  axios.put(base_url + "/guest/" + id, { name, address });
+  axios.put(base_url + "api/guest/" + id, { name, address });
 
 export const updateArriving = (id, isArrived) =>
-  axios.put(base_url + "/guest/" + id, { isArrived: !isArrived });
+  axios.put(base_url + "api/guest/" + id, { isArrived: !isArrived });
 
 export const updateSpreading = (id, isSpread) =>
-  axios.put(base_url + "/guest/" + id, { isSpread: !isSpread });
+  axios.put(base_url + "api/guest/" + id, { isSpread: !isSpread });
